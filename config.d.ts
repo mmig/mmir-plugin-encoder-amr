06f929a130b2f9f243076120c1f64c0f8cda51f7
russa <aaron.russ@dfki.de>
2019-02-14 17:40:16 +0100

    add typings & config meta-data

---
+++ config.d.ts
@@ -0,0 +1,10 @@
+
+/**
+ * TODO add/support AMR encoder settings,
+ * e.g. mode [0,8] (DEFAULT: 7)
+ *
+ * NOTE would need to be implemented via mmir-plugin-encoder-core(?)
+ */
+export interface AMREncoderConfigEntry {
+  audioEncoder?: any;
+}
---
+++ module-config.js
@@ -0,0 +1,4 @@
+
+//  NA
+
+modules.export = {};
